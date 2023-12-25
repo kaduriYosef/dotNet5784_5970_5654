79fd955c69247d24385f43d5f1ad38c0c528e502
Kaduri Yosef <kaduri184@gmail.com>
2023-12-25 15:40:25 +0200

    initial code + Welcome function

---
+++ Stage0/Program5970.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Targil0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome5970();
+            Welcome5654();
+            Console.ReadKey();
+        }
+
+        static partial void Welcome5654();
+        private static void Welcome5970()
+        {
+            Console.WriteLine("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", name);
+        }
+    }
+}
