450ee8396f3cfee6da20a1f813ad17778778b29a
ImanuelPerel <imanuel.p222@gmail.com>
2023-12-26 18:50:47 +0200

    initial code + welcome function

---
+++ Stage0/Program5654.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Targil0
+{
+    partial class Program5654
+    {
+        static partial void Welcome5654()
+        {
+            Console.WriteLine("i am also here, Emanuel");
+        }
+    }
+}
