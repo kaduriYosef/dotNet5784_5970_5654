b01594908edee7c15f410e2c9f01c36313a464eb
ImanuelPerel <imanuel.p222@gmail.com>
2023-12-26 19:08:42 +0200

    added declaration

--- Stage0/Program5654.cs
+++ Stage0/Program5654.cs
@@ -8,6 +8,7 @@ namespace Targil0
 {
     partial class Program5654
     {
+        static partial void Welcome5654();
         static partial void Welcome5654()
         {
             Console.WriteLine("i am also here, Emanuel");
