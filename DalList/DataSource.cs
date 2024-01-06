7560d9b6bb6bb67fe59bd4d89c47b73fea3da3ed
Kaduri Yosef <kaduri184@gmail.com>
2024-01-07 00:03:11 +0200

    fix the Config class

--- DalList/DataSource.cs
+++ DalList/DataSource.cs
@@ -5,9 +5,10 @@ internal static class DataSource
 {
     internal static class Config
     {
-        internal const int startCourseId = 1000;
-        private static int nextCourseId = startCourseId;
-        internal static int NextCourseId { get => nextCourseId++; }
+        //for task class
+        internal const int startTaskId = 0;
+        private static int nextTaskId = startTaskId;
+        internal static int NextTaskId { get => nextTaskId++; }
     }
     internal static List<DO.Task> Tasks { get; } = new();
     internal static List<DO.Engineer> Engineers { get; } = new();
