318820604e32f58a6e742a6d22255df6b03fd484
Kaduri Yosef <kaduri184@gmail.com>
2024-01-06 23:42:05 +0200

    until Config

---
+++ DalList/DataSource.cs
@@ -0,0 +1,16 @@
+﻿
+namespace Dal;
+
+internal static class DataSource
+{
+    internal static class Config
+    {
+        internal const int startCourseId = 1000;
+        private static int nextCourseId = startCourseId;
+        internal static int NextCourseId { get => nextCourseId++; }
+    }
+    internal static List<DO.Task> Tasks { get; } = new();
+    internal static List<DO.Engineer> Engineers { get; } = new();
+
+
+}
