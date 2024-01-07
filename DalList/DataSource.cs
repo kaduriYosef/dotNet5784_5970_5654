7984b20160f724bd0777d151aee898db8ff4e40f
Kaduri Yosef <kaduri184@gmail.com>
2024-01-07 14:17:08 +0200

    fix dataSourch

--- DalList/DataSource.cs
+++ DalList/DataSource.cs
@@ -9,6 +9,11 @@ internal static class DataSource
         internal const int startTaskId = 0;
         private static int nextTaskId = startTaskId;
         internal static int NextTaskId { get => nextTaskId++; }
+
+        //for Dependency class
+        internal const int startDependencyId = 0;
+        private static int nextDependencyId = startDependencyId;
+        internal static int NextDependencyId { get => nextDependencyId++; }
     }
     internal static List<DO.Task> Tasks { get; } = new();
     internal static List<DO.Engineer> Engineers { get; } = new();
