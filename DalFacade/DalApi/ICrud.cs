32c83098c01daf752b81060d8f1616d68d7c8ef6
Kaduri Yosef <kaduri184@gmail.com>
2024-01-14 15:45:17 +0200

    level 2 from episoder 1 - 5

---
+++ DalFacade/DalApi/ICrud.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DO;
+
+namespace DalApi;
+
+public interface ICrud<T> where T : class
+{
+    //Creates new entity object in DAL
+    int Create(T item);
+
+    //Reads entity object by its ID 
+    T? Read(int id);
+
+    //stage 1 only, Reads all entity objects
+    List<T> ReadAll();
+
+    //Updates entity object
+    void Update(T item);
+
+    //Deletes an object by its Id
+    void Delete(int id);
+
+
+}
--- DalFacade/DalApi/IDependency.cs
+++ DalFacade/DalApi/IDependency.cs
@@ -3,23 +3,23 @@ using DO;
 
 namespace DalApi;
 
-public interface IDependency
+public interface IDependency : ICrud<Dependency>
 {
-    //Creates new entity object in DAL
-    int Create(Dependency item); 
-    
-    //Reads entity object by its ID 
-    Dependency? Read(int id); 
-   
-    //stage 1 only, Reads all entity objects
-    List<Dependency> ReadAll(); 
-    
-    //Updates entity object
-    void Update(Dependency item); 
-    
-    //Deletes an object by its Id
-    void Delete(int id); 
-    
+    ////Creates new entity object in DAL
+    //int Create(Dependency item);
+
+    ////Reads entity object by its ID 
+    //Dependency? Read(int id);
+
+    ////stage 1 only, Reads all entity objects
+    //List<Dependency> ReadAll();
+
+    ////Updates entity object
+    //void Update(Dependency item);
+
+    ////Deletes an object by its Id
+    //void Delete(int id);
+
     //show whether there exist such a dependency
-    public bool DoesExist(int dependent_id, int dependsOn_id); 
+    public bool DoesExist(int dependent_id, int dependsOn_id);
 }
