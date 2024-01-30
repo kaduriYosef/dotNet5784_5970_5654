16793adf64ccafb99665f84e11d89098241c72f3
Kaduri Yosef <kaduri184@gmail.com>
2024-01-30 18:00:24 +0200

    for bug

--- BL/BlApi/IEngineer.cs
+++ BL/BlApi/IEngineer.cs
@@ -1,14 +1,19 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace BlApi;
 
-public interface IEngineer
+internal interface IEngineer
 {
-    public int Create(BO.Engineer item);
+    public int Create(BO.Engineer boEndineer);
     public BO.Engineer? Read(int id);
-    public IEnumerable<BO.Engineer> ReadAll(Func<BO.Engineer, bool> filter=null);
-    public void Update(BO.Engineer item);
+    public IEnumerable<BO.Engineer> ReadAll();
+    public void Update(BO.Engineer boEngineer);
     public void Delete(int id);
-    
-    
+
+    public BO.EngineerInTask(int engineerId,int taskId); //to implement
+
 }
