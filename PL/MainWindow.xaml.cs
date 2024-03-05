0b87fc2f27e6983d1438c39de1cdec72c09622c2
Imanuel David Perel <imanuel.p222@gmail.com>
2024-03-05 19:24:32 +0200

    fix bug

--- PL/MainWindow.xaml.cs
+++ PL/MainWindow.xaml.cs
@@ -20,12 +20,30 @@ namespace PL
     public partial class MainWindow : Window
     {
 
+        private void clock_run() 
+        { 
+            Thread.Sleep(1000);
+            Time += new TimeSpan(1, 0, 0, 0);
+        }
         static readonly BlApi.IBl s_bl = BlApi.Factory.Get();
         public MainWindow()
         {
             InitializeComponent();
+            new Thread(clock_run).Start();
         }
-       
+
+
+
+        public TimeSpan Time      
+        {
+            get { return (TimeSpan)GetValue(TimeProperty); }
+            set { SetValue(TimeProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Time.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty TimeProperty =
+            DependencyProperty.Register("Time", typeof(TimeSpan), typeof(MainWindow), new PropertyMetadata(new TimeSpan(0,0,0,0)));
+
 
 
         private void Button_Admin(object sender, RoutedEventArgs e)
