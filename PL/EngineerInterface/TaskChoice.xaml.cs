58c4bc8fd3d0a44960b6b5e37bf1c12515403392
Imanuel David Perel <imanuel.p222@gmail.com>
2024-03-24 01:02:17 +0200

    more engineer interfface ready

--- PL/EngineerInterface/TaskChoice.xaml.cs
+++ PL/EngineerInterface/TaskChoice.xaml.cs
@@ -23,49 +23,49 @@ namespace PL.EngineerInterface
   // Access the business logic layer
         static readonly BlApi.IBl s_bl = BlApi.Factory.Get();
 
-    // Dependency property for binding the list of tasks
-    //public IEnumerable<BO.TaskInList> TaskList
-    //{
-    //    get { return (IEnumerable<BO.TaskInList>)GetValue(TaskListProperty); }
-    //    set { SetValue(TaskListProperty, value); }
-    //}
-    //public static readonly DependencyProperty TaskListProperty =
-    //    DependencyProperty.Register("TaskList", typeof(IEnumerable<BO.TaskInList>), typeof(TaskForEngineer), new PropertyMetadata(null));
+        // Dependency property for binding the list of tasks
+        public IEnumerable<BO.TaskInList> TaskList
+        {
+            get { return (IEnumerable<BO.TaskInList>)GetValue(TaskListProperty); }
+            set { SetValue(TaskListProperty, value); }
+        }
+        public static readonly DependencyProperty TaskListProperty =
+            DependencyProperty.Register("TaskList", typeof(IEnumerable<BO.TaskInList>), typeof(TaskChoice), new PropertyMetadata(null));
 
-    // Current engineer for whom the tasks are being displayed
-    public BO.Engineer currentEngineer = new BO.Engineer();
+        // Current engineer for whom the tasks are being displayed
+        public BO.Engineer currentEngineer = new BO.Engineer();
 
     // Constructor for TaskForEngineer window
     public TaskChoice(BO.Engineer CurrentEngineer)
     {
         InitializeComponent();
         // Retrieve tasks based on engineer's level and availability
-        //TaskList = s_bl.Task.ReadAll(item => (int)item.Complexity <= (int)CurrentEngineer.Level && item.Engineer.Id == null && item.StartDate == null);
+        TaskList = s_bl.Task.ReadAllSimplified(item => (int)item.Complexity <= (int)CurrentEngineer.Level && item.Engineer == null && item.StartDate == null);
         currentEngineer = CurrentEngineer;
     }
 
     // Event handler for selecting a task from the list
     private void ChooseTask_Button(object sender, MouseButtonEventArgs e)
     {
-        // Check if a schedule of tasks has been set up
-        //if (s_bl.Dates.getStartProject() == null)
-        //{
-        //    MessageBox.Show("A schedule of tasks needs to be set up first");
-        //    return;
-        //}
-        //// Get the selected task
-        //BO.TaskInList? task = (sender as ListView)?.SelectedItem as BO.TaskInList;
-        //// Close the current window and open the window to add task for engineer
-        //Close();
-        //new AddTaskForEngineer(currentEngineer, task.Id).ShowDialog();
-    }
+            // Check if a schedule of tasks has been set up
+            if (BO.Tools.StartDateOrNull() == null)
+            {
+                MessageBox.Show("A schedule of tasks needs to be set up first");
+                return;
+            }
+            // Get the selected task
+            BO.TaskInList? task = (sender as ListView)?.SelectedItem as BO.TaskInList;
+            // Close the current window and open the window to add task for engineer
+            Close();
+            //new AddTaskForEngineer(currentEngineer, task.Id).ShowDialog();
+        }
 
     // Event handler for the Home button click
     private void Home_Click(object sender, RoutedEventArgs e)
     {
-        // Close the current window and open the EngineerView window for the current engineer
+        // Close the current window and open the EngineerInterface main window for the current engineer
         Close();
-        //new EngineerView(currentEngineer.Id).ShowDialog();
+        new EngineerInterfaceMainWindow(currentEngineer.Id).Show();
     }
 }
 }
